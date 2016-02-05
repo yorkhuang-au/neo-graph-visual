a445b14d5218062b95892fe3f7246b1118af00ba
York Huang <york.y.huang@gmail.com>
2016-02-05 16:02:26 +1100

    update

--- public/ng-menu1.js
+++ public/ng-menu1.js
@@ -7,9 +7,42 @@ $(function(){
   var layoutPadding = 50;
   var layoutDuration = 500;
 
-  var graphP = loadGraphData();
-  var styleP = loadStyleData();
-  var infoTemplate = getInfoHandleTemplate();
+  // get exported json from cytoscape desktop via ajax
+  var graphP = $.ajax({
+    url: 'https://cdn.rawgit.com/maxkfranz/3d4d3c8eb808bd95bae7/raw', // wine-and-cheese.json
+    type: 'GET',
+    dataType: 'json'
+  });
+
+  // also get style via ajax
+  var styleP = $.ajax({
+    url: 'https://cdn.rawgit.com/maxkfranz/9210c03a591f8736b82d/raw', // wine-and-cheese-style.cycss
+    type: 'GET',
+    dataType: 'text'
+  });
+
+  Handlebars.registerHelper('toJson', function(data){
+    var str='';
+
+    for (var key in data ) {
+      //if (data.hasOwnProperty(key)) {
+      str += '<p class="ac-node-type"><i class="fa fa-info-circle"></i> ' + key + ': ' + data.key + '</p>'
+        //str =  str +  key //+ ': '+ data.key 
+      //}
+    }
+
+    return str; //JSON.stringify(obj ); //str;
+  });
+
+
+
+  var infoTemplate = Handlebars.compile([
+    '<p class="ac-name">[{{id}}]{{name}}</p>',
+    '<p class="ac-node-type"><i class="fa fa-info-circle"></i> {{NodeTypeFormatted}} {{#if Type}}({{Type}}){{/if}}</p>',
+    '{{#if Milk}}<p class="ac-milk"><i class="fa fa-angle-double-right"></i> {{Milk}}</p>{{/if}}',
+    '{{#if Country}}<p class="ac-country"><i class="fa fa-map-marker"></i> {{Country}}</p>{{/if}}',
+    '<p class="ac-more"><i class="fa fa-external-link"></i> <a target="_blank" href="http://google.com/search?q={{name}}">More information</a></p>'
+  ].join(''));
 /*
   var infoTemplate = Handlebars.compile([
     //'<p class="ac-node-type"><i class="fa fa-info-circle"></i> {{toJson this}} </p>'
